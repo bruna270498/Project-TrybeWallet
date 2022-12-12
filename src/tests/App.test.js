f8518669db0402a52769507feb62fccd17275575
Bruna <brunaalmeida075@gmail.com>
2022-12-12 15:47:48 -0300

    testes do requisito 5 ok

---
+++ src/tests/App.test.js
@@ -0,0 +1,43 @@
+import { screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import App from '../App';
+
+describe('Testando a aplicação', () => {
+  it('Verificando a pagina de login', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const loginTitulo = screen.getByRole('heading', { name: /login/i });
+    expect(loginTitulo).toBeInTheDocument();
+    expect(loginTitulo.innerHTML).toBe('Login');
+
+    const inputEmail = screen.getByRole('textbox');
+    expect(inputEmail.type).toBe('email');
+    expect(inputEmail).toBeInTheDocument();
+
+    const inputSenha = screen.getByPlaceholderText(/digite sua senha/i);
+    expect(inputSenha.type).toBe('password');
+    expect(inputSenha).toBeInTheDocument();
+
+    const botao = screen.getByRole('button', { name: /entrar/i });
+    expect(botao).toBeInTheDocument();
+    expect(botao.innerHTML).toBe('Entrar');
+  });
+  it('Testando que botão é habilitado quando email for digitado correto e senhar tiver acima de 6 e se ao cicar no botão muda para rota /carteira', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const inputEmail = screen.getByRole('textbox');
+    const inputSenha = screen.getByPlaceholderText(/digite sua senha/i);
+    const botao = screen.getByRole('button', { name: /entrar/i });
+
+    expect(botao.disabled).toBe(true);
+
+    fireEvent.change(inputEmail, { target: { value: 'alguem@alguem.com' } });
+    fireEvent.change(inputSenha, { target: { value: '123456' } });
+
+    userEvent.click(botao);
+    expect(botao.disabled).toBe(false);
+
+    expect(history.location.pathname).toBe('/carteira');
+  });
+});
